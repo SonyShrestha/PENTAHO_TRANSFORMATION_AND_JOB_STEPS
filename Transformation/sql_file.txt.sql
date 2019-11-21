62559ffa325706a0580f316b12737c4e7e6225a4
sony <sony.sth8@gmail.com>
2019-11-21 20:16:52 +0545

    GIT PENTAHO TRANSFORMATION AND JOB STEPS

---
+++ Transformation/sql_file.txt.sql
@@ -0,0 +1,9 @@
+CREATE TABLE film_actor
+(
+  id INT
+, name TINYTEXT
+, address TINYTEXT
+)
+;
+
+INSERT INTO film_actor(id, name, address) VALUES (1,'sony','ktm');INSERT INTO film_actor(id, name, address) VALUES (2,'hari','bkt');
